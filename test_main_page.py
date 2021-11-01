11b1d9883b3d9fca49e3081bbab92e5c85e16ccf
Mikhail Tortsev <59667317+flashm123@users.noreply.github.com>
2021-11-02 01:17:52 +0300

    Add files via upload

---
+++ test_main_page.py
@@ -0,0 +1,25 @@
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+import pytest
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/category/books/non-fiction/hacking_7/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_not_be_items_in_basket()
+        basket_page.should_be_text_cart_is_empty()
